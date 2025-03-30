9c9a617c410e4cccfc230746dbe2a24f7868e6a8
Adonay  Aragón <aaragon@trainingroots.xyz>
2025-03-30 07:58:11 -0600

    Add user routing to TortisalRouter; implement UserPage for user details

--- src/router/TortisalRouter.jsx
+++ src/router/TortisalRouter.jsx
@@ -5,6 +5,7 @@ import { Navbar } from "../ui";
 import { ProductsPage } from "../products/pages/ProductPage";
 import { EmployeesPage } from "../employees/pages/EmployeesPage";
 import { ToolsIssuedPage } from "../tools-issued/pages/ToolsIssuedPage";
+import { UserPage } from "../users/pages/UserPage";
 
 export const TortisalRouter = () => {
     const { logged } = useContext(AuthContext);
@@ -19,7 +20,9 @@ export const TortisalRouter = () => {
             <Routes>
                 <Route path="/" element={<ProductsPage />}></Route>
                 <Route path="employees" element={<EmployeesPage/>}></Route>
-                <Route path="user" element={<ProductsPage />}></Route>
+                <Route path="employees:[id]" element={<UserPage/>}></Route>
+                <Route path="users" element={<UserPage/>}></Route>
+                <Route path="users:[id]" element={<UserPage/>}></Route>
                 <Route path="tools-issued" element={<ToolsIssuedPage />}></Route>
                 <Route path="shift" element={<ProductsPage />}></Route>
             </Routes>
